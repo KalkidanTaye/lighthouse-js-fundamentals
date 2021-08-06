d75f557fd9e61f64624a708b553111d755e29d59
KalkidanTaye <kali.taye@gmail.com>
2021-08-06 01:52:16 +0000

    Array of Packing list of kitchen

--- packing-list.js
+++ packing-list.js
@@ -10,6 +10,14 @@ const packingList = [
 ];
 
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+let i = 0;
+for (i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+console.log("Kitchen stuff to pack:");
+let y = 0;
+while (y < packingList.length) {
+  console.log(packingList[y]);
+  y++;
 }
